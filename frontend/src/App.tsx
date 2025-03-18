d9f5435931f9f5b11d74db1ba0a58fb3fb3ee2b5
Tamojit Das <tamojitdas181007@gmail.com>
2025-03-18 21:55:44 +0530

    Landing page UI update: 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ import ParticleBackground from "./components/ParticleBackground";
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-br from-blue-800 to-black">
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black">
         <ParticleBackground />
 
         <Routes>
--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -1,7 +1,26 @@
+import About from "../../components/landing/About";
+import LandingNavbar from "../../components/navbars/LandingNavbar";
+
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <>
+      <LandingNavbar />
+
+      <div className="pt-20 lg:pt-32 p-4 flex flex-col items-center justify-center w-full h-screen bg-[url('/bg.jpeg')] bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 bg-black/65"></div>
+
+        <div className="relative z-10 flex items-center justify-center flex-col gap-3">
+          <img src="/Logo.png" alt="logo" className="w-48 -mt-20 -mb-8" />
+          <h1 className="text-primary text-[80px] lg:text-[100px]">FinGPT</h1>
+          <p className="text-subhead text-center max-w-lg">
+            A One-stop solution for all your finance, economics & investment doubts & automation.
+          </p>
+        </div>
+      </div>
+
+      <About />
+    </>
   )
 }
 
-export default Landing
+export default Landing;
